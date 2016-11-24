ba98cf3fbde83ed6269643e5300c96ebc7c69a1b
cvh <namerp@googlemail.com>
2016-11-24 22:57:14 +0100

    cleanup: remove create archive scripts

--- create_tar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git archive --format=tar.gz --prefix=script.config.vdr-$1/ tags/$1 -o script.config.vdr-$1.tar.gz
--- create_zip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git archive --format=zip --prefix=script.config.vdr/ tags/$1 -o script.config.vdr-$1.zip
